581634c2805349b7bc5dc4b288972d280055c697
kraken <manbibg@gmail.com>
2023-06-17 22:52:51 +0700

    Second commit: MainActivity.java, dialog_instruction.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.myapplication;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.PersistableBundle;
+import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowMetrics;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,17 +24,19 @@ import com.example.myapplication.databinding.DialogInstructionBinding;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    static int scrWidth = 0, scrHeight = 0, tapCounter1 = 0, tapCounter2 = 0;
     RelativeLayout.LayoutParams lp;
     ActivityMainBinding binding;
+    SharedPreferences sp;
+
     static boolean gameStart = false, won = false, preparingPhase = false;
+    static int scrWidth = 0, scrHeight = 0, tapCounter1 = 0, tapCounter2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        // initialize block height
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         scrWidth = dm.widthPixels;
@@ -35,19 +44,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lp = (RelativeLayout.LayoutParams) binding.layoutBlock2.getLayoutParams();
         lp.height = scrHeight/2;
         binding.layoutBlock2.setLayoutParams(lp);
-
+        // set listener
         binding.relativeLayout1.setOnClickListener(this);
         binding.layoutBlock2.setOnClickListener(this);
+        // get shared preferences and show the dialog
+        sp = getPreferences(Context.MODE_PRIVATE);
+        showInstruction();
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-
+    void showInstruction(){
+        boolean dontShowDialog = sp.getBoolean("DONT_SHOW_INSTRUCTION_DIALOG", false);
+        if(dontShowDialog) return;
         DialogInstructionBinding db = DialogInstructionBinding.inflate(LayoutInflater.from(this), null, false);
         Dialog dialog = new Dialog(this);
         dialog.setContentView(db.getRoot());
         db.btnClose.setOnClickListener(v -> dialog.dismiss());
+        db.checkBox.setOnCheckedChangeListener((compoundButton, b) -> {
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putBoolean("DONT_SHOW_INSTRUCTION_DIALOG", b);
+            editor.apply();
+        });
         dialog.show();
     }
 
@@ -63,15 +79,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     void showWinner(){
         binding.txtBanner.setVisibility(View.VISIBLE);
-        String winner = tapCounter1 > tapCounter2 ? "P1 WIN" : "P2 WIN";
-        binding.txtBanner.setText(winner + "\n" + "Tap to continue..");
-        preparingPhase = true;
+        String winner = tapCounter1 > tapCounter2 ? "P1 WON" : "P2 WON";
+        binding.txtBanner.setText(winner + "\n" + "Counter: " + Math.max(tapCounter1, tapCounter2));
+    }
+
+    void increase(Object obj){
+        int increment = 25;
+        int currentHeight = binding.layoutBlock2.getHeight();
+        if(obj.equals("P1")){
+            currentHeight = Math.max(currentHeight - increment, 0);
+            tapCounter1++;
+            if(currentHeight == 0) won = true;
+        }
+        else{
+            currentHeight = Math.min(currentHeight + increment, scrHeight);
+            tapCounter2++;
+            if(currentHeight == scrHeight) won = true;
+        }
+        lp.height = currentHeight;
+        binding.layoutBlock2.setLayoutParams(lp);
     }
 
     @Override
     public void onClick(View view) {
-        int increment = 25;
-//        binding.layoutBlock2.bringToFront();
+        int interval = 3000;
+
         if(preparingPhase){
             startPreparingPhase();
             return;
@@ -84,22 +116,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
-        int currentHeight = binding.layoutBlock2.getHeight();
-        if(view.getTag().equals("P1")){
-            currentHeight = Math.max(currentHeight - increment, 0);
-            tapCounter1++;
-            if(currentHeight == 0) won = true;
-        }
-        else{
-            currentHeight = Math.min(currentHeight + increment, scrHeight);
-            tapCounter2++;
-            if(currentHeight == scrHeight) won = true;
-        }
-        lp.height = currentHeight;
-        binding.layoutBlock2.setLayoutParams(lp);
+        increase(view.getTag());
 
         if(won){
             showWinner();
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                preparingPhase = true;
+                binding.txtBanner.setText("Tap to continue..");
+            }, interval);
         }
     }
 }
